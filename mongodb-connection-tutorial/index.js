c025dfbf7d341fc34bddc6164a9dfde9febbddba
ahsanMBP <ahsan1246@gmail.com>
2023-12-11 01:42:22 +0500

    brakedown the db connection and get data method for understanding. (11 Dec 2023 - 1:42 AM)

--- mongodb-connection-tutorial/index.js
+++ mongodb-connection-tutorial/index.js
@@ -3,10 +3,14 @@ const mongodbUrl = "mongodb://localhost:27017";
 const mongodbName = "ecommerce-app";
 const mongoClient = new MongoClient(mongodbUrl);
 
-async function getData() {
+async function dbConnection() {
   let result = await mongoClient.connect();
   let db = result.db(mongodbName);
-  let collection = db.collection("products");
+  return db.collection("products");
+}
+
+async function getData() {
+  let collection = await dbConnection();
   let response = await collection.find({}).toArray();
   console.log(response);
 }
