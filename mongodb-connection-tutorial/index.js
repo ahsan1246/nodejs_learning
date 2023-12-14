46facbbec5481dca6c13b80555e550730129f59f
Ahsan (MBA) <ahsan1246@gmail.com>
2023-12-14 12:44:10 +0500

    promises and async and await method implementation for dbconnection and fetch data and all stuff. (14 Dec 2023 - 12:44 PM)

--- mongodb-connection-tutorial/index.js
+++ mongodb-connection-tutorial/index.js
@@ -1,6 +1,14 @@
 const dbConnection = require("./mongodb.js");
 
 async function getData() {
+  // old pattern
+  // dbConnection().then((collection) => {
+  //   collection.find().toArray().then((data) => {
+  //     console.log(data);
+  //   });
+  // });
+
+  // New way of promises
   let collection = await dbConnection();
   let response = await collection.find({}).toArray();
   console.log(response);
